660eb86dd2ef373cc9f9654cf311df6e17e72b8d
PGTALK\pgtalk <conqueror89107@gmail.com>
2022-09-28 18:18:09 +0800

    初始專案

---
+++ src/main/java/com/my/scorpio/ScorpioServerApplication.java
@@ -0,0 +1,13 @@
+package com.my.scorpio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScorpioServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScorpioServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/my/scorpio/ScorpioServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.scorpio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScorpioServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
