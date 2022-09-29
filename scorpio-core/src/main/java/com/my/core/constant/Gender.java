dd98be5879823d7f7a2dd0bdce1a44a5232d528e
PGTALK\pgtalk <conqueror89107@gmail.com>
2022-09-29 13:39:03 +0800

    refactor(refactor core package name)

---
+++ scorpio-core/src/main/java/com/my/core/constant/Gender.java
@@ -0,0 +1,29 @@
+package com.my.core.constant;
+
+import lombok.Getter;
+
+@Getter
+public enum Gender {
+    Female(0, "女"),
+    Male(1, "男"),
+    Other(2, "其它"),
+    ;
+
+    private int code;
+    private String desc;
+
+    Gender(int code, String desc) {
+        this.code = code;
+        this.desc = desc;
+    }
+
+    /**
+     * 根據desc獲取Code .
+     */
+    public static Gender getCode(String desc) {
+        for (Gender s : Gender.values()) {
+            if (s.getDesc().equals(desc)) return s;
+        }
+        throw new IllegalArgumentException("查無此描述");
+    }
+}
---
+++ scorpio-core/src/main/java/com/my/core/util/excel/ExcelException.java
@@ -0,0 +1,10 @@
+package com.my.core.util.excel;
+
+/**
+ * 建立 Excel 的錯誤
+ */
+public class ExcelException extends Exception {
+    public ExcelException(String str) {
+        super(str);
+    }
+}
