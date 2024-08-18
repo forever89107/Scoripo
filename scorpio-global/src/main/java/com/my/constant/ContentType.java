86bf0dc7e4bfb8e698ee3ca8228430bbc09ce6e9
Liam_Weng <conqueror89107@gamil.com>
2024-08-18 14:46:23 +0800

    feature global constant

---
+++ scorpio-global/src/main/java/com/my/constant/ContentType.java
@@ -0,0 +1,12 @@
+package com.my.constant;
+
+@SuppressWarnings("unused")
+public class ContentType {
+
+    public final static String JSON = "application/json";
+    public final static String PNG = "image/png";
+    public final static String ZIP = "application/x-zip-compressed";
+    public final static String CSV = "application/csv";
+    public final static String HTML = "text/html;charset=utf-8";
+
+}
---
+++ scorpio-global/src/main/java/com/my/constant/DateConstant.java
@@ -0,0 +1,58 @@
+package com.my.constant;
+
+public class DateConstant {
+
+    public static class SECOND {
+
+        /**
+         * Number of seconds in a day
+         */
+        public static final int DAY = 86400;
+
+        /**
+         * Number of seconds in half an hour
+         */
+        public static final int HALF_HOUR = 1800;
+
+        /**
+         * Number of seconds in 10 minutes
+         */
+        public static final int TEN_MINUTES = 600;
+
+        /**
+         * Number of seconds in 1 hour
+         */
+        public static final int ONE_HOUR = 3600;
+
+        /**
+         * Number of seconds in 2 hours
+         */
+        public static final int TWO_HOURS = 7200;
+
+    }
+
+    public static class MILLISECOND {
+
+        /**
+         * Number of milliseconds in 10 minutes
+         */
+        public static final int TEN_MINUTES = 600000;
+
+        /**
+         * Number of milliseconds in 5 minutes
+         */
+        public static final int FIVE_MINUTES = 300000;
+
+        /**
+         * Number of milliseconds in 1 minute
+         */
+        public static final int ONE_MINUTE = 60000;
+
+        /**
+         * Number of milliseconds in a day
+         */
+        public static final int ONE_DAY = 86400000;
+
+    }
+
+}
